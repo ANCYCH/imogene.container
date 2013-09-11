05df74c905d0bd88bae0b5e405beab2a19011fb9
Julien Dupouy <julien.dupouy.fr@gmail.com>
2013-09-11 19:39:59 +0200

    Format sources

--- org.imogene.rcp.derby/src/org/imogene/rcp/derby/Activator.java
+++ org.imogene.rcp.derby/src/org/imogene/rcp/derby/Activator.java
@@ -7,15 +7,15 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
-	
+
 	private static final Logger logger = Logger.getLogger(Activator.class.getName());
-	
+
 	private static BundleContext sContext;
 
 	static BundleContext getContext() {
 		return sContext;
 	}
-	
+
 	private NetworkServerControl mNetworkServerControl;
 
 	@Override
@@ -25,11 +25,11 @@ public class Activator implements BundleActivator {
 		mNetworkServerControl = new NetworkServerControl();
 		mNetworkServerControl.start(null);
 	}
-	
+
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.sContext = null;
-		
+
 		mNetworkServerControl.shutdown();
 	}
 
--- org.imogene.rcp.initializer/src/org/imogene/rcp/initializer/Activator.java
+++ org.imogene.rcp.initializer/src/org/imogene/rcp/initializer/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator {
 
 	private static final Logger logger = Logger.getLogger(Activator.class.getName());
-	
+
 	private static BundleContext context;
 
 	static BundleContext getContext() {
@@ -21,17 +21,17 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		logger.info("Initialize properties");
 		Activator.context = bundleContext;
-		
+
 		// Derby properties
 		System.setProperty("derby.system.home", createPath("derby"));
-		
+
 		// Jetty properties
 		System.setProperty("jetty.home", createPath("jetty"));
 
 		// Web apps properties
 		System.setProperty("imogene.binary.path", createPath("binaries"));
 		System.setProperty("imogene.identity.path", createPath("identities"));
-		
+
 		// Synchronization properties
 		System.setProperty("sync.home", Platform.getInstanceLocation().getURL().getFile() + "sync");
 	}
@@ -40,7 +40,7 @@ public class Activator implements BundleActivator {
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 	}
-	
+
 	private static String createPath(String folder) {
 		File file = new File(Platform.getInstanceLocation().getURL().getFile(), folder);
 		if (!file.exists()) {
